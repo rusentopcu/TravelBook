b3ab5721febf0539f153acf81f6cf75f3065db99
Rusen Topcu <rusentopcu@msn.com>
2020-02-04 16:44:49 +0300

    Project Created

---
+++ TravelBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravelBook
+//
+//  Created by Rusen Topcu on 4.02.2020.
+//  Copyright © 2020 Rusen Topcu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
