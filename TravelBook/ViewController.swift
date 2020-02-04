7aa4d8e09e607a621a8a1f6d1adfbad42f38968c
Rusen Topcu <rusentopcu@msn.com>
2020-02-04 18:24:41 +0300

    User's location determine

--- TravelBook/ViewController.swift
+++ TravelBook/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDelegate {
 
+    //MARK: - Outlet
+    @IBOutlet weak var mapView: MKMapView!
+    
+    //MARK: - Object create for Location info request
+    var location = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
+        location.delegate = self
+        //Lokasyonun keskinlik ayarı
+        location.desiredAccuracy = kCLLocationAccuracyBest
+        //Kullanıcıdan konum bilgisinin ne zaman alınacağı
+        location.requestWhenInUseAuthorization()
+        //Kullanıcının yerini almaya başlamak için
+        location.startUpdatingLocation()
     }
-
+    
+    //MARK: - Functions
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        var location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        var span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        var region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+    }
 
 }
 
