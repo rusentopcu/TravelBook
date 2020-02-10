70f3257201e7f7ba3bbda27e63f8e57eb099b789
Rusen Topcu <rusentopcu@msn.com>
2020-02-10 13:03:41 +0300

    Show the data

--- TravelBook/ListViewController.swift
+++ TravelBook/ListViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import  CoreData
 
 class ListViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
 
-    
+    var titleArray = [String]()
+    var idArray = [UUID]()
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -21,6 +23,8 @@ class ListViewController: UIViewController, UITableViewDelegate,UITableViewDataS
         tableView.dataSource = self
        
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+        getData()
     }
     
     @objc func addButtonClicked() {
@@ -28,13 +32,50 @@ class ListViewController: UIViewController, UITableViewDelegate,UITableViewDataS
     }
     
         func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+            return titleArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Test"
+        cell.textLabel?.text = titleArray[indexPath.row]
         return cell
+    }
+    
+    func getData() {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let contex = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Places")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try contex.fetch(fetchRequest)
+            if results.count > 0 {
+                
+                titleArray.removeAll(keepingCapacity: false)
+                idArray.removeAll(keepingCapacity: false)
+                
+                for result in results as! [NSManagedObject] {
+                    
+                    if let title = result.value(forKey: "title") as? String {
+                        titleArray.append(title)
+                    }
+                    if let id = result.value(forKey: "id") as? UUID {
+                        idArray.append(id)
+                    }
+                    tableView.reloadData()
+                    
+                }
+            
+            }
+            
+        }
+        catch {
+            print("error")
+        }
+        
+        
     }
 
 }
